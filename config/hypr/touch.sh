1b83fabaf611c963f57e9c1ce928ce3b6c6a225e
Aboba <komarfascket@gmail.com>
2024-09-03 20:34:31 +0400

    new file:   bocchi.png 	new file:   config/hypr/hyprland.conf 	new file:   config/hypr/hyprpaper.conf 	new file:   config/hypr/touch.sh 	new file:   config/mako/config 	new file:   config/waybar/config.jsonc 	new file:   config/waybar/style.css 	new file:   config/wofi/config 	new file:   config/wofi/style.css

---
+++ config/hypr/touch.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+HYPRLAND_DEVICE="syn1b8b:01-06cb:7da2-touchpad"
+HYPRLAND_VARIABLE="device[$HYPRLAND_DEVICE]:enabled"
+
+if [ -z "$XDG_RUNTIME_DIR" ]; then
+  export XDG_RUNTIME_DIR=/run/user/$(id -u)
+fi
+
+export STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
+
+enable_touchpad() {
+    printf "true" >"$STATUS_FILE"
+  notify-send -u normal "Enabling Touchpad"
+hyprctl keyword $HYPRLAND_VARIABLE "true" -r
+}
+
+disable_touchpad() {
+    printf "false" >"$STATUS_FILE"
+notify-send -u normal "Disabling Touchpad"
+hyprctl keyword $HYPRLAND_VARIABLE "false" -r
+}
+
+if ! [ -f "$STATUS_FILE" ]; then
+  enable_touchpad
+else
+  if [ $(cat "$STATUS_FILE") = "true" ]; then
+    disable_touchpad
+  elif [ $(cat "$STATUS_FILE") = "false" ]; then
+    enable_touchpad
+  fi
+fi
